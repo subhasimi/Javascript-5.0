0930c522245f0d418a5d93471fc6947a440f86ba
SUBHASMITA SAHOO <139224945+subhasimi@users.noreply.github.com>
2024-06-28 17:13:18 +0000

    done

--- 02-intermediate-js/arrays.js
+++ 02-intermediate-js/arrays.js
@@ -48,13 +48,22 @@
 
 //2.for a given array with prices of 5 items->[250,645,300,900,50].all items have an offer of 10% off on them. change the array to store final price after applying offer.
 
-let items=[250,645,300,900,50];
-let i=0;
-for(let val of items){
-    console.log(`value at index ${i}=${val}`);
-    let offer=val/10;
-    items[i]=items[i]-offer;
-    console.log(`value after offer =${items[i]}`);
-    i++;
+// let items=[250,645,300,900,50];
+// let i=0;
+// for(let val of items){
+//     console.log(`value at index ${i}=${val}`);
+//     let offer=val/10;
+//     items[i]=items[i]-offer;
+//     console.log(`value after offer =${items[i]}`);
+//     i++;
 
-}
+// }
+//or//
+
+// let prices = [250, 645, 300, 900, 50];
+
+// for (let i = 0; i < prices.length; i++) {
+//   prices[i] = prices[i] * (90/100) 
+// }
+
+// console.log(prices);
